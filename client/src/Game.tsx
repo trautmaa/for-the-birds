ea46c78833ac0691e49498447d3caa76d09d9616
Alex T <alexjtrautman@gmail.com>
2020-04-09 15:46:29 -0400

    Success message

--- client/src/Game.tsx
+++ client/src/Game.tsx
@@ -11,6 +11,9 @@ import { backendUrl } from './App';
 export const Game: React.FC<{ moduleId?: string }> = ({ moduleId }) => {
 	const [ currentStep, setCurrentStep ] = React.useState(0);
 	const [ questions, setQuestions ] = React.useState<Question[]>([]);
+	const numerator = currentStep;
+	const denominator = questions.length;
+	const moduleInProgress = numerator !== denominator;
 
 	const increment = () => {
 		const nextStep = currentStep + 1;
@@ -29,11 +32,11 @@ export const Game: React.FC<{ moduleId?: string }> = ({ moduleId }) => {
 		<div className="App">
 			<SVGRender currentStep={currentStep} />
 			<div className="game-contents">
-				<Progress numerator={currentStep} denominator={questions.length} />
-				<Input action={increment} question={questions[currentStep]} />
-				<div>
-					<button onClick={increment}>Skip question</button>
-				</div>
+				<Progress numerator={numerator} denominator={denominator} />
+				{(moduleInProgress && <Input action={increment} question={questions[currentStep]} />) || (
+					<div>Congratulations, you finished</div>
+				)}
+				<div className="skip-question">{moduleInProgress && <button onClick={increment}>Skip</button>}</div>
 				<a className="attribution-link" href="https://www.vecteezy.com/free-vector/landscape">
 					Landscape Vectors by Vecteezy
 				</a>
